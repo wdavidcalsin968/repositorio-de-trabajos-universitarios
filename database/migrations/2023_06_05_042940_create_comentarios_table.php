ee9cc8ec704075022bb955774a662d0af430f582
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-06-04 23:43:11 -0500

    realizar migraciones

---
+++ database/migrations/2023_06_05_042940_create_comentarios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('revision_id')->constrained('revisions')->onDelete('cascade');
+            $table->longText('descripcion');
+            $table->string('posicion');
+            $table->datetime('fecha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comentarios');
+    }
+};
